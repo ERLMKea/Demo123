08a5f837455498880b10413d64d381c7a581febe
erlm <erlm@kea.dk>
2024-08-27 17:28:20 +0200

    hje

---
+++ src/test/java/org/example/demo123/Demo123ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.demo123;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo123ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
